79a90cb137025c7b12050319c2129ac584870425
Carmc99 <carmc99@gmail.com>
2025-02-16 16:33:46 -0500

    Delete loggin

--- SquadNET.Application/ServiceCollectionExtension.cs
+++ SquadNET.Application/ServiceCollectionExtension.cs
@@ -1,7 +1,13 @@
-﻿namespace SquadNET.Application
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace SquadNET.Application
 {
     public static class ServiceCollectionExtension
     {
-
+        public static IServiceCollection AddSquadApplication(this IServiceCollection services)
+        {
+            return services;
+        }
     }
 }
