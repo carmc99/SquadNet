0e081847a957b39f5aeb6135a4ad1e89f9549dd7
Carlos Medina C <carlos.medina@arandasoft.com>
2025-02-21 16:01:11 -0500

    Fix error swaggergen

--- SquadNET.API/Program.cs
+++ SquadNET.API/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.CustomSchemaIds(type => type.FullName?.Replace("+", "."));
+});
 builder.Services.AddSquadApplication();
 
 var app = builder.Build();
